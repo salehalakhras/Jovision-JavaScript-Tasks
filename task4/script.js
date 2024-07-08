bb28c270ae2970f69c06d756c03433d462e2c7b0
Saleh Alakhras <salehalakhras2000@gmail.com>
2024-07-08 10:55:04 +0300

    finished task 4

---
+++ task4/script.js
@@ -0,0 +1,25 @@
+
+function createArr() {
+  let arr = Array(101);
+
+  for (let i = 0; i < 101; i++) {
+    arr[i] = i;
+  }
+
+  return arr;
+}
+
+function printSum(arr){
+    let sum = 0;
+    for(let i=0;i<arr.length;i++){
+        sum += arr[i];
+    }
+    console.log(sum);
+}
+
+btn = document.querySelector('.btn');
+
+btn.addEventListener('click', e => {
+    arr = createArr();
+    printSum(arr);
+});
